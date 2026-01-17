125c6ad90ea5a13e03d5668c5b55d2c548f63c50
Casper V. Kristensen <casper@vkristensen.dk>
2026-01-17 21:28:55 +0100

    completely done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import postgres from "postgres";
+import process from "node:process";
+import { Hono } from "hono";
 
-const app = new Hono()
+const sql = postgres({
+  host: "database",
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+await sql`
+  create table if not exists "sheets" ( sheet jsonb );
+`;
+await sql`
+  create index idxsheets ON sheets using gin ( sheet );
+`;
 
-export default app
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
