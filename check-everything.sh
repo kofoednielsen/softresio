51a44bbbaddc219d0e0396b56cde664ab287a965
Jeppe Thorup <jepptp@gmail.com>
2026-01-25 00:11:02 +0100

    json validation

--- check-everything.sh
+++ check-everything.sh
@@ -3,3 +3,9 @@ deno check --config frontend/deno.json frontend/src
 deno check --config backend/deno.json backend/src
 deno fmt --config frontend/deno.json frontend/src
 deno fmt --config backend/deno.json backend/src
+for f in backend/instances/*.json; do
+  if ! jq -e '.' "$f" > /dev/null; then
+    echo "$f is invalid"
+    exit 1
+  fi
+done
