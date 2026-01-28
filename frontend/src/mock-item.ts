f60f49d5e1517391da4ec1a00574a511b027bbe0
Jeppe Thorup <jepptp@gmail.com>
2026-01-28 03:02:36 +0100

    mock item

---
+++ frontend/src/mock-item.ts
@@ -0,0 +1,11 @@
+import type { Item } from "../types/types.ts"
+export const nothingItem: Item = {
+  id: 0,
+  tooltip: "This item has no tooltip",
+  icon: "",
+  name: "No item reserved",
+  slot: "",
+  type: "",
+  classes: [],
+  quality: 1,
+}
